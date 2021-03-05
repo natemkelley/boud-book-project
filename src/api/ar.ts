52011ae8f3ccf85c6951e7ac7f80a05eca31fc93
n8kel <nate.kelley@digicert.com>
2021-03-05 08:55:24 -0700

    BOUD BOOK SCRUBBER LIVE

--- src/api/ar.ts
+++ src/api/ar.ts
@@ -2,11 +2,12 @@ import queryString from "query-string";
 import axios from "axios";
 import { ARResult } from "./interfaces";
 
-const LOCAL_API = "http://localhost:8080";
+//const LOCAL_API = "http://localhost:8080";
+const BOUD_BOOK_SCRUBBER = "https://boudbookscrubber.duckdns.org";
 
 const createArQuery = (title: string, author: string) => {
   const query = queryString.stringify({ title, author });
-  return `${LOCAL_API}?${query}`;
+  return `${BOUD_BOOK_SCRUBBER}?${query}`;
 };
 
 export const getPoints = async (title: string, author: string) =>

1453a4a92fc12f8dc92c6c76b63ce0ad53f992e1
n8kel <nate.kelley@digicert.com>
2021-03-05 08:59:55 -0700

    Replace index

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,176 @@
 <template>
   <div>
-    <v-text-field
-      v-model="value"
-      label="Book Search"
-      @keydown="search"
-    ></v-text-field>
+    <v-row no-gutters>
+      <v-col cols="4" class=" px-2 search">
+        <div class="d-flex">
+          <v-text-field
+            v-model="value"
+            label="Google Book Search List"
+            :loading="loading"
+            @keydown="search"
+          ></v-text-field>
+          <v-btn
+            class="ma-3"
+            :loading="loading"
+            color="primary"
+            @click="loader = 'loading'"
+          >
+            Search
+          </v-btn>
+        </div>
+      </v-col>
 
-    <div class="d-flex align-center test">
-      <v-card
-        v-for="item in items"
-        :key="item.id"
-        width="344"
-        :height="350"
-        class="my-card"
-      >
-        <v-card-text>
-          <div>{{ item.volumeInfo.authors }}</div>
-          <p class="display-1 text--primary">
-            {{ item.volumeInfo.title }}
-          </p>
-          <div class="text--primary">
-            {{ item.volumeInfo.description }}
-          </div>
-        </v-card-text>
-        <v-card-actions>
-          <code>
-            {{ item.arData || "loading" }}
-          </code>
-        </v-card-actions>
-      </v-card>
-    </div>
+      <v-col cols="8">
+        <div class="mx-3">
+          <v-card
+            v-for="book in booksWithColors"
+            :key="book.id"
+            :color="book.color.color"
+            dark
+            class="mt-3"
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div class="d-flex flex-column">
+                <v-card-title
+                  class="headline"
+                  v-text="book.title"
+                ></v-card-title>
+                <v-card-subtitle
+                  v-for="author in book.authors"
+                  :key="author"
+                  v-text="author"
+                ></v-card-subtitle>
+                <v-card-text class="description">
+                  {{ book.description }}
+                </v-card-text>
+              </div>
+
+              <div class="ma-3 mr-4">
+                <v-img
+                  v-if="book.images.thumbnail"
+                  :width="150"
+                  gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+                  :src="book.images.thumbnail"
+                ></v-img>
+              </div>
+            </div>
+            <v-card-actions class="d-flex align-center justify-space-between">
+              <v-btn
+                v-if="book.previewLink"
+                class="ml-2 my-1"
+                :href="book.previewLink"
+                outlined
+                rounded
+                small
+              >
+                More Info
+              </v-btn>
+              <div class="pills">
+                <v-btn
+                  v-for="pill in createPills(book)"
+                  :key="pill"
+                  class="ml-2 my-1"
+                  outlined
+                  rounded
+                  small
+                >
+                  {{ pill }}
+                </v-btn>
+
+                <v-btn
+                  v-if="book.loadingARData"
+                  class="ml-2 my-1"
+                  outlined
+                  rounded
+                  small
+                >
+                  <div class="mr-1">AR DATA</div>
+                  <v-progress-circular
+                    :size="14"
+                    :width="2"
+                    indeterminate
+                    color="white"
+                  ></v-progress-circular>
+                </v-btn>
+              </div>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
+
+    <div class="d-flex align-center book list"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import debounce from "lodash/debounce";
-import findIndex from "lodash/findIndex";
-import { getPoints, getBookList, test } from "api/index";
+import { namespace } from "vuex-class";
+import ISO6391 from "iso-639-1";
+import uniqolor from "uniqolor";
+import { getModule } from "vuex-module-decorators";
+import bookModule, { bookModuleName } from "store/books";
+import { Book } from "src/applicationInterfaces";
+
+const books = namespace(bookModuleName);
 
 @Component
 export default class HomePage extends Vue {
   value = "";
-  items: any = {};
+  loading = false;
+  @books.State("books") books: Book[];
 
   get search() {
-    return debounce(this.performSearch, 1500);
+    return debounce(this.searchList, 500);
   }
 
-  async performSearch() {
-    const result = await getBookList(this.value);
-    this.items = result.items;
-
-    this.items.forEach((result: any) => {
-      const title = result.volumeInfo.title;
-      const author =
-        result.volumeInfo?.authors && result.volumeInfo?.authors[0];
-      const id = result.id;
-
-      this.getArScores(title, author, id);
-    });
+  get booksWithColors() {
+    return this.books.map(book => ({
+      ...book,
+      color: uniqolor(book.id, {
+        saturation: [35, 75],
+        lightness: 20,
+        differencePoint: 30,
+      }),
+    }));
   }
 
-  async getArScores(title: string, author: string, id: string) {
-    /*  const result = await getPoints(title, author);
-    const indexOfResult = findIndex(this.items, { id });
+  createPills(book: Book) {
+    const returnArray = [];
+    if (book.language) returnArray.push(ISO6391.getName(book.language));
+    if (book.countryOfOrigin) returnArray.push(book.countryOfOrigin);
+    if (book.publicDomain) returnArray.push("Public Domain");
+    if (book.pageCount) returnArray.push(`${book.pageCount} Pages`);
+    if (book.publishYear) returnArray.push(`Published: ${book.publishYear}`);
+    if (book.points) returnArray.push(`${book.points} Points`);
+    if (book.level) returnArray.push(`Level ${book.level}`);
 
-    if (indexOfResult > -1 && result.isExactMatch) {
-      this.items[indexOfResult] = {
-        ...this.items[indexOfResult],
-        arData: result,
-      };
-    }*/
+    return returnArray;
   }
 
-  async mounted() {
-    test();
+  async searchList() {
+    this.loading = true;
+    await getModule(bookModule, this.$store).getGoogleBookList(this.value);
+    this.loading = false;
   }
 }
 </script>
 
 <style scoped>
-.test {
-  display: flex;
-  flex-wrap: wrap;
+.search {
+  min-height: 85vh;
+  border-right: 1px solid rgb(180, 180, 180);
 }
 
-.my-card {
-  margin: 10px;
+.description {
+  max-height: 200px;
   overflow: auto;
+}
+
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
 }
 </style>
